4f3d17c22468141d0649477d4efb24930113b179
wac <wabaham9@gmail.com>
2023-11-15 17:45:45 +0300

    Add authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  before_validation :set_default_posts_counter
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :posts, foreign_key: :author_id
   has_many :comments
   has_many :likes
@@ -8,5 +14,11 @@ class User < ApplicationRecord
 
   def recent_three_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  private
+
+  def set_default_posts_counter
+    self.posts_counter ||= 0
   end
 end
